7f1a7fa03698f2716c245bfcdda509054a49eb5b
Daniil Tsarev <daniil.tsaryov@jetbrains.com>
2024-11-13 21:04:15 +0100

    Initial commit

---
+++ src/test/java/org/example/cashcard/CashcardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cashcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashcardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
