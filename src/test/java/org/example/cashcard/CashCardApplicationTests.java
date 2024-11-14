2eb064c7f90881b65cd5cf739a2e503c7a5ac1fe
Daniil Tsarev <daniil.tsaryov@jetbrains.com>
2024-11-14 21:27:24 +0100

    Add support for listing all CashCard objects
    Implemented `findAll` endpoint in `CashCardController` to retrieve all CashCard objects. Added corresponding tests to validate serialization and deserialization of CashCard arrays, and adjusted existing tests and resources accordingly.

--- src/test/java/org/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/org/example/cashcard/CashCardApplicationTests.java
@@ -2,18 +2,23 @@ package org.example.cashcard;
 
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.net.URI;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.test.annotation.DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@DirtiesContext(classMode = AFTER_EACH_TEST_METHOD)
 class CashCardApplicationTests {
 
     @Autowired
@@ -40,7 +45,6 @@ class CashCardApplicationTests {
         assertThat(response.getBody()).isBlank();
     }
 
-
     @Test
     void shouldCreateANewCashCard() {
         CashCard newCashCard = new CashCard(null, 250.00);
@@ -58,5 +62,21 @@ class CashCardApplicationTests {
 
         assertThat(id).isNotNull();
         assertThat(amount).isEqualTo(250.00);
+    }
+
+    @Test
+    void shouldReturnAllCashCardsWhenListIsRequested() {
+        ResponseEntity<String> response = restTemplate.getForEntity("/cashcards", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        int cashCardCount = documentContext.read("$.length()");
+        assertThat(cashCardCount).isEqualTo(3);
+
+        JSONArray ids = documentContext.read("$..id");
+        assertThat(ids).containsExactlyInAnyOrder(99, 100, 101);
+
+        JSONArray amounts = documentContext.read("$..amount");
+        assertThat(amounts).containsExactlyInAnyOrder(123.45, 1.0, 150.00);
     }
 }
